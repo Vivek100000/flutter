b2c8ed42f1407950a71a2f9bf47cc354aac47ebd
Vivek100000 <94130401+Vivek100000@users.noreply.github.com>
2022-08-15 23:42:09 +0530

    latest commit

--- result.dart
+++ result.dart
@@ -3,9 +3,24 @@ import 'package:flutter/cupertino.dart';
 class result extends StatelessWidget {
   final int total_score;
   result(this.total_score);
+  String get resultPhrase {
+    var resultText = 'you did it';
+    if (total_score > 20) {
+      resultText = 'good work your score is: $total_score';
+    } else if (total_score > 40) {
+      resultText = 'excellent work your score is: $total_score';
+    } else {
+      resultText = 'need improvement your score is: $total_score';
+    }
+    return resultText;
+  }
+
   Widget build(BuildContext context) {
     return Center(
-      child: Text('assesment complete total score: $total_score'),
+      child: Text(
+        resultPhrase,
+        style: TextStyle(fontSize: 24, fontWeight: FontWeight.normal),
+      ),
     );
   }
 }
